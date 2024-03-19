544161478ee183673f5836f8d2d81f493b620ecb
Sahu, Rajesh Chandan <rajeshchandan.sahu@lowes.com>
2024-03-19 20:33:46 +0530

    chk

--- src/com/lowes/web/controller/impl/ProductControllerImpl.java
+++ src/com/lowes/web/controller/impl/ProductControllerImpl.java
@@ -1,6 +1,7 @@
-package com.lowes.web.controller;
+package com.lowes.web.controller.impl;
 
 import com.google.gson.Gson;
+import com.lowes.web.controller.LowesWebServiceController;
 import com.lowes.web.exceptions.InputValidationException;
 import com.lowes.web.services.ProductService;
 import com.lowes.web.util.AppUtil;
@@ -11,41 +12,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import wt.log4j.LogR;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
-@SwaggerDefinition(
-        info = @Info(
-                description = "This is a sample server",
-                version = "1.0.0",
-                title = "Swagger Sample Servlet",
-                termsOfService = "http://swagger.io/terms/",
-                contact = @Contact(
-                        name = "Sponge-Bob",
-                        email = "apiteam@swagger.io",
-                        url = "http://swagger.io"
-                ),
-                license = @License(
-                        name = "Apache 2.0",
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
-                )
-        ),
-        consumes = {"application/json", "application/xml"},
-        produces = {"application/json", "application/xml"},
-        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS}
-)
-@Api(
-        value = "/lowes",
-        tags = {"Lowes custom Services"}
-)
-@Path("/lowes")
-public class ProductWebServiceController {
-    private static final Logger logger = LogR.getLogger(ProductWebServiceController.class.getName());
+public class ProductControllerImpl extends LowesWebServiceController {
+
+    private static final Logger logger = LogR.getLogger(ProductControllerImpl.class.getName());
     private static final JSONParser parser = new JSONParser();
     private static final AppUtil util = new AppUtil();
     private static final Gson gson = new Gson();
@@ -84,8 +58,8 @@ public class ProductWebServiceController {
         try {
 
             if (input == null || "".equalsIgnoreCase(input)) {
-                logger.log(Level.DEBUG, "input data (var2)>>>: {} ", input);
-                throw new InputValidationException("Enter a valid Input in the body");
+                logger.log(Level.DEBUG, "input records>>>: {} ", input);
+                throw new InputValidationException("Enter a valid Input Record in the body");
             }
 
             JSONObject inputJson = (JSONObject) parser.parse(input);
@@ -137,12 +111,81 @@ public class ProductWebServiceController {
         try {
 
             if (input == null || "".equalsIgnoreCase(input)) {
-                logger.log(Level.DEBUG, "input data (var2)>>>: {} ", input);
+                logger.log(Level.DEBUG, "input data >>>: {} ", input);
                 throw new InputValidationException("Enter a valid Input in the body");
             }
 
             JSONObject inputJson = (JSONObject) parser.parse(input);
             json = new ProductService().deleteSeasonRecords(inputJson);
+
+        } catch (InputValidationException var9) {
+            var7 = util.getExceptionJson(var9.getMessage());
+            return Response.status(400).entity(gson.toJson(var7)).build();
+        } catch (Exception var11) {
+            logger.error(var11.getMessage(), var11);
+            var7 = util.getExceptionJson(var11.getMessage());
+            return Response.status(400).entity(gson.toJson(var7)).build();
+        }
+
+        return Response.status(200).entity(gson.toJson(json)).build();
+    }
+
+    @ApiOperation(
+            httpMethod = "POST",
+            value = "Update records from system bases on input criteria",
+            notes = "Returns the results for the execution of a query builder report."
+                    + "<br><br>Example"
+                    + "<br>Sample payload for passing parameters: "
+                    + "<br>{"
+                    + "\"searchCriteria\": {"
+                    + "\"key1\","
+                    + "\"key2\""
+                    + "},"
+                    + "\"data\": ["
+                    + "{"
+                    + "\"key1\": \"value1\","
+                    + "\"key2\": \"value2\","
+                    + "\"key3\": \"value3\""
+                    + "}"
+                    + "]"
+                    + "}",
+            response = JSONObject.class
+    )
+    @ApiResponses({@ApiResponse(
+            code = 200,
+            message = "Success",
+            response = JSONObject.class
+    ), @ApiResponse(
+            code = 404,
+            message = "Invalid uri"
+    ), @ApiResponse(
+            code = 500,
+            message = "Unexpected error"
+    )})
+    @ApiImplicitParams({@ApiImplicitParam(
+            name = "CSRF_NONCE",
+            value = "The CSRF nonce as returned from the /security/csrf endpoint.  See the Swagger documentation titled CSRF Protection for more information.",
+            required = true,
+            dataType = "string",
+            paramType = "HEADER"
+    )})
+    @POST
+    @Path("/product/updateRecords")
+    @Produces({"application/json"})
+    @Consumes({"application/json"})
+    public Response updateRecords(@Context HttpHeaders var1, String input) {
+        JSONObject json;
+
+        JSONObject var7;
+        try {
+
+            if (input == null || "".equalsIgnoreCase(input)) {
+                logger.log(Level.DEBUG, "input data >>>: {} ", input);
+                throw new InputValidationException("Enter a valid Input in the body");
+            }
+
+            JSONObject inputJson = (JSONObject) parser.parse(input);
+            json = new ProductService().update(inputJson);
 
         } catch (InputValidationException var9) {
             var7 = util.getExceptionJson(var9.getMessage());
